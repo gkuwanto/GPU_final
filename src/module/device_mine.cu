0d3a5e0dcef6066dfb52388cc79531e1d4eeeddd
Garry Kuwanto <gkuwanto@gmail.com>
2021-04-12 16:29:42 +0700

    debug

--- src/module/device_mine.cu
+++ src/module/device_mine.cu
@@ -17,13 +17,13 @@ uint32_t CPU_mine(string payload, uint32_t difficulty) {
     return 0;
 }
 
-__global__ void GPU_naive_mine(char *payload, uint32_t difficulty, uint32_t* nonce, bool* nonce_found) {
+__global__ void GPU_naive_mine(char *payload, uint32_t *difficulty, uint32_t* nonce, bool* nonce_found) {
     if (!nonce_found[0]){
         uint32_t candidate_nonce = gridDim.x*blockDim.x*blockIdx.y + blockDim.x*blockIdx.x + threadIdx.x;
         stringstream ss;
         ss << payload << hex << candidate_nonce;
         string hash = hash_sha256(ss.str());
-        if (hash.substr(0, difficulty) == string(difficulty, '0')){
+        if (hash.substr(0, *difficulty) == string(*difficulty, '0')){
             nonce[0] = candidate_nonce;
             nonce_found[0] = true;
         }
@@ -40,7 +40,7 @@ uint32_t device_mine_dispatcher(string payload, uint32_t difficulty, MineType re
         case MineType::MINE_NAIVE: {
             uint32_t *nonce;
             bool *nonce_found;
-            char * c_payload;
+            char const * c_payload;
             uint32_t *dev_dif;
             cudaMallocManaged(&nonce, sizeof(uint32_t));
             cudaMallocManaged(&dev_dif, sizeof(uint32_t));
@@ -51,7 +51,7 @@ uint32_t device_mine_dispatcher(string payload, uint32_t difficulty, MineType re
             *dev_dif = difficulty;
             int blockSize = 1024;
             int numBlocks = 2; //4194304; // ceil(0xffffffff/1024)
-            GPU_naive_mine<<numBlocks, blockSize>>(c_payload, difficulty, nonce, nonce_found);
+            GPU_naive_mine<<numBlocks, blockSize>>(c_payload, dev_dif, nonce, nonce_found);
             cudaDeviceSynchronize();
             return nonce[0];
         }
