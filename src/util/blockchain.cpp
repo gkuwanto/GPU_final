0f6e74a9c42402ca0aa066f77ddfb4655f2321c2
Garry Kuwanto <gkuwanto@gmail.com>
2021-04-19 21:44:59 +0700

    f

--- src/util/blockchain.cpp
+++ src/util/blockchain.cpp
@@ -44,35 +44,89 @@ Block::Block(CandidateBlock c_block, uint32_t nonce) {
 string Block::getHashableString() {
     return this->candidate_block.getHashableString();
 }
-void Block::calculateHash(unsigned char *hash) {
-    string payload = this->candidate_block.getHashableString();
+
+void change_non(SHA256_CTX *ctx, uint32_t nonce){
+    const uint32_t k[64] = {
+		0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
+		0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
+		0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
+		0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
+		0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
+		0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
+		0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
+		0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+	};
 
     char *b = "0123456789abcdef";
 	unsigned char *a = (unsigned char *)b;
-	unsigned char nonce_byte[8] = {
+	BYTE data[8] = {
 		a[((nonce >> 28) % 16)], a[((nonce >> 24) % 16)], a[((nonce >> 20) % 16)], 
 		a[((nonce >> 16) % 16)], a[((nonce >> 12) % 16)], a[((nonce >> 8)  % 16)],
 		a[((nonce >> 4)  % 16)], a[(nonce % 16)]
 	};
 
-	unsigned char *data = new unsigned char[payload.length() + 9];
-    std::copy( payload.begin(), payload.end(), data );
+	WORD i;
 
-	data[payload.length()+0] = nonce_byte[0];
-	data[payload.length()+1] = nonce_byte[1];
-	data[payload.length()+2] = nonce_byte[2];
-	data[payload.length()+3] = nonce_byte[3];
-	data[payload.length()+4] = nonce_byte[4];
-	data[payload.length()+5] = nonce_byte[5];
-	data[payload.length()+6] = nonce_byte[6];
-	data[payload.length()+7] = nonce_byte[7];
-	data[payload.length()+8] = 0;
+	for (i = 0; i < 8; ++i) {
+		ctx->data[ctx->datalen] = data[i];
+		ctx->datalen++;
+		if (ctx->datalen == 64) {
+			WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
 
+			for (i = 0, j = 0; i < 16; ++i, j += 4)
+				m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
+			for ( ; i < 64; ++i)
+				m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
 
-    SHA256_CTX ctx;
+			a = ctx->state[0];
+			b = ctx->state[1];
+			c = ctx->state[2];
+			d = ctx->state[3];
+			e = ctx->state[4];
+			f = ctx->state[5];
+			g = ctx->state[6];
+			h = ctx->state[7];
+
+			for (i = 0; i < 64; ++i) {
+				t1 = h + EP1(e) + CH(e,f,g) + k[i] + m[i];
+				t2 = EP0(a) + MAJ(a,b,c);
+				h = g;
+				g = f;
+				f = e;
+				e = d + t1;
+				d = c;
+				c = b;
+				b = a;
+				a = t1 + t2;
+			}
+
+			ctx->state[0] += a;
+			ctx->state[1] += b;
+			ctx->state[2] += c;
+			ctx->state[3] += d;
+			ctx->state[4] += e;
+			ctx->state[5] += f;
+			ctx->state[6] += g;
+			ctx->state[7] += h;
+			ctx->bitlen += 512;
+			ctx->datalen = 0;
+		}
+	}
+
+}
+void Block::calculateHash(unsigned char *hash) {
+    string payload = this->candidate_block.getHashableString();
+    unsigned char *data = new unsigned char[payload.length() + 1];
+            
+	std::copy( payload.begin(), payload.end(), data );
+	data[payload.length()] = 0;
+	
+
+	SHA256_CTX ctx;
 	sha256_init(&ctx);
-	sha256_update(&ctx, (unsigned char *) data, payload.length()+8);	//ctx.state contains a-h
+	sha256_update(&ctx, (unsigned char *) data, payload.length());	//ctx.state contains a-h
 
+    change_non(&ctx, nonce);
     sha256_final(&ctx, hash);
     
 }
@@ -87,7 +141,7 @@ bool Block::verify_nonce(){
     int i=0;
     while(hash[i] == difficulty[i])
         i++;
-    return (hash[i] < difficulty[i]);
+    return (i>=nDifficulty);
 }
 
 Blockchain::Blockchain() {
