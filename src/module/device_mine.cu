7f640610bfe3d14370f3042a6f3c651919266df5
Garry Kuwanto <gkuwanto@gmail.com>
2021-04-19 20:33:29 +0700

    debug

--- src/module/device_mine.cu
+++ src/module/device_mine.cu
@@ -202,7 +202,9 @@ uint32_t CPU_mine(std::string payload, uint32_t difficulty) {
 		hash[7] = ENDIAN_SWAP_32(h + 0x5be0cd19);
 
 		unsigned char *hhh = (unsigned char *) hash;
-		cout<<hash;
+		cout<<hash<<endl;
+		cout<<(&ctx)->difficulty<<endl;
+
 		break;
 		i=0;
 		while(hhh[i] == (&ctx)->difficulty[i])
