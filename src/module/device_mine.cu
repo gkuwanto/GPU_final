8122e079c92a553d4bba9a21d80d0e8a7c0ae603
Garry Kuwanto <gkuwanto@gmail.com>
2021-04-19 21:16:33 +0700

    change gpu mine

--- src/module/device_mine.cu
+++ src/module/device_mine.cu
@@ -120,7 +120,7 @@ uint32_t CPU_mine(std::string payload, uint32_t difficulty) {
 	std::copy( payload.begin(), payload.end(), data );
 	data[payload.length()] = 0;
 	
-	int i, j;
+	int i;
 
 	Nonce_result h_nr;
 	initialize_nonce_result(&h_nr);
@@ -135,21 +135,12 @@ uint32_t CPU_mine(std::string payload, uint32_t difficulty) {
 		unsigned char hash[32];
 		sha256_change_nonce(&ctx, nonce);
 		sha256_final(&ctx, hash);
-		cout<<hash[i]<<endl;
-		cout<<(&ctx)->difficulty[i]<<endl;
 
 		i=0;
 		while(hash[i] == (&ctx)->difficulty[i])
 			i++;
-		cout<<i;
 		if(hash[i] < (&ctx)->difficulty[i]) {
-			//Synchronization Issue
-			//Kind of a hack but it really doesn't matter which nonce
-			//is written to the output, they're all winners :)
-			//Further it's unlikely to even find a nonce let alone 2
 			(&h_nr)->nonce_found = true;
-			//The nonce here has the correct endianess,
-			//but it must be stored in the block in little endian order
 			(&h_nr)->nonce = nonce;
 			return h_nr.nonce;
 		}
@@ -158,93 +149,147 @@ uint32_t CPU_mine(std::string payload, uint32_t difficulty) {
 }
 
 __global__ void GPU_mine(SHA256_CTX *ctx, Nonce_result *nr ) {
-	const uint32_t k[64] = {
-		0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
-		0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
-		0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
-		0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
-		0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
-		0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
-		0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
-		0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
-	};
-    unsigned int m[64];
-    unsigned int hash[8];
-	unsigned int a,b,c,d,e,f,g,h,t1,t2;
-	int i, j;
 	uint32_t nonce = gridDim.x*blockDim.x*blockIdx.y + blockDim.x*blockIdx.x + threadIdx.x;
     sha256_change_nonce(ctx, nonce);
-    unsigned int *le_data = (unsigned int *) ctx->data;
-	for(i=0; i<16; i++)
-		m[i] = le_data[i];
-    
+	unsigned char hash[32];
+
+	// pad
+
+	i = ctx->datalen;
+
+	//Pad whatever data is left in the buffer.
+	//If it's less than 56 bytes, 8 bytes required for bit length l
+	//For this application we are always the first case
+	if (ctx->datalen < 56) {
+		ctx->data[i++] = 0x80;
+		while (i < 56)
+			ctx->data[i++] = 0x00;
+	}
+	//Otherwise, pad with 0s and store l in its own message block
+	else {
+		ctx->data[i++] = 0x80;
+		while (i < 64)
+			ctx->data[i++] = 0x00;
+		// transform
+		WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
+
+		for (i = 0, j = 0; i < 16; ++i, j += 4)
+			m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
+		for ( ; i < 64; ++i)
+			m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
+
+		a = ctx->state[0];
+		b = ctx->state[1];
+		c = ctx->state[2];
+		d = ctx->state[3];
+		e = ctx->state[4];
+		f = ctx->state[5];
+		g = ctx->state[6];
+		h = ctx->state[7];
+
+		for (i = 0; i < 64; ++i) {
+			t1 = h + EP1(e) + CH(e,f,g) + k[i] + m[i];
+			t2 = EP0(a) + MAJ(a,b,c);
+			h = g;
+			g = f;
+			f = e;
+			e = d + t1;
+			d = c;
+			c = b;
+			b = a;
+			a = t1 + t2;
+		}
+
+		ctx->state[0] += a;
+		ctx->state[1] += b;
+		ctx->state[2] += c;
+		ctx->state[3] += d;
+		ctx->state[4] += e;
+		ctx->state[5] += f;
+		ctx->state[6] += g;
+		ctx->state[7] += h;
+
+		// end transform
+		memset(ctx->data, 0, 56);
+	}
+
+	//Store value of l
+	ctx->bitlen += ctx->datalen * 8;
+	ctx->data[63] = ctx->bitlen;
+	ctx->data[62] = ctx->bitlen >> 8;
+	ctx->data[61] = ctx->bitlen >> 16;
+	ctx->data[60] = ctx->bitlen >> 24;
+	ctx->data[59] = ctx->bitlen >> 32;
+	ctx->data[58] = ctx->bitlen >> 40;
+	ctx->data[57] = ctx->bitlen >> 48;
+	ctx->data[56] = ctx->bitlen >> 56;
+
+	// transform
+
+	WORD a, b, c, d, e, f, g, h, j, t1, t2, m[64];
+
+	for (i = 0, j = 0; i < 16; ++i, j += 4)
+		m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
 	for ( ; i < 64; ++i)
-        m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
-
-    a = ctx->state[0];
-    b = ctx->state[1];
-    c = ctx->state[2];
-    d = ctx->state[3];
-    e = ctx->state[4];
-    f = ctx->state[5];
-    g = ctx->state[6];
-    h = ctx->state[7];
-
-    SHA256_COMPRESS_8X
-
-	//Prepare input for next SHA-256
-	m[0] = a + ctx->state[0];
-	m[1] = b + ctx->state[1];
-	m[2] = c + ctx->state[2];
-	m[3] = d + ctx->state[3];
-	m[4] = e + ctx->state[4];
-	m[5] = f + ctx->state[5];
-	m[6] = g + ctx->state[6];
-	m[7] = h + ctx->state[7];
-	//Pad the input
-	m[8] = 0x80000000;	
-	for(i=9; i<15; i++)
-		m[i] = 0x00;
-	m[15] = 0x00000100;	//Write out l=256
-	for (i=16 ; i < 64; ++i)
 		m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
 
-	//Initialize the SHA-256 registers
-	a = 0x6a09e667;
-	b = 0xbb67ae85;
-	c = 0x3c6ef372;
-	d = 0xa54ff53a;
-	e = 0x510e527f;
-	f = 0x9b05688c;
-	g = 0x1f83d9ab;
-	h = 0x5be0cd19;
+	a = ctx->state[0];
+	b = ctx->state[1];
+	c = ctx->state[2];
+	d = ctx->state[3];
+	e = ctx->state[4];
+	f = ctx->state[5];
+	g = ctx->state[6];
+	h = ctx->state[7];
 
-	SHA256_COMPRESS_1X
+	for (i = 0; i < 64; ++i) {
+		t1 = h + EP1(e) + CH(e,f,g) + k[i] + m[i];
+		t2 = EP0(a) + MAJ(a,b,c);
+		h = g;
+		g = f;
+		f = e;
+		e = d + t1;
+		d = c;
+		c = b;
+		b = a;
+		a = t1 + t2;
+	}
 
-	hash[0] = ENDIAN_SWAP_32(a + 0x6a09e667);
-	hash[1] = ENDIAN_SWAP_32(b + 0xbb67ae85);
-	hash[2] = ENDIAN_SWAP_32(c + 0x3c6ef372);
-	hash[3] = ENDIAN_SWAP_32(d + 0xa54ff53a);
-	hash[4] = ENDIAN_SWAP_32(e + 0x510e527f);
-	hash[5] = ENDIAN_SWAP_32(f + 0x9b05688c);
-	hash[6] = ENDIAN_SWAP_32(g + 0x1f83d9ab);
-	hash[7] = ENDIAN_SWAP_32(h + 0x5be0cd19);
+	ctx->state[0] += a;
+	ctx->state[1] += b;
+	ctx->state[2] += c;
+	ctx->state[3] += d;
+	ctx->state[4] += e;
+	ctx->state[5] += f;
+	ctx->state[6] += g;
+	ctx->state[7] += h;
 
-    unsigned char *hhh = (unsigned char *) hash;
+	// end transform
+
+
+	WORD i;
+
+	for (i = 0; i < 4; ++i) {
+		hash[i]      = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 4]  = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 8]  = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 12] = (ctx->state[3] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 16] = (ctx->state[4] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 20] = (ctx->state[5] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 24] = (ctx->state[6] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 28] = (ctx->state[7] >> (24 - i * 8)) & 0x000000ff;
+	}
+
+	// Check hash
 	i=0;
-	while(hhh[i] == ctx->difficulty[i])
+	while(hash[i] == ctx->difficulty[i])
 		i++;
-
-	if(hhh[i] < ctx->difficulty[i]) {
-		//Synchronization Issue
-		//Kind of a hack but it really doesn't matter which nonce
-		//is written to the output, they're all winners :)
-		//Further it's unlikely to even find a nonce let alone 2
+	if(hash[i] < ctx->difficulty[i]) {
 		nr->nonce_found = true;
-		//The nonce here has the correct endianess,
-		//but it must be stored in the block in little endian order
 		nr->nonce = nonce;
 	}
+	
+    
 }
 
 uint32_t device_mine_dispatcher(std::string payload, uint32_t difficulty, MineType reduction_type) {
@@ -267,17 +312,7 @@ uint32_t device_mine_dispatcher(std::string payload, uint32_t difficulty, MineTy
             sha256_init(&ctx);
             sha256_update(&ctx, (unsigned char *) data, payload.length());	//ctx.state contains a-h
             set_difficulty(ctx.difficulty, difficulty);
-			unsigned int *le_data = (unsigned int *)ctx.data;
-			unsigned int le;
-			int i, j;
-			for(i=0, j=0; i<16; i++, j+=4) {
-				//Get the data out as big endian
-				//Store it as little endian via x86
-				//On the device side cast the pointer as int* and dereference it correctly
-				le = (ctx.data[j] << 24) | (ctx.data[j + 1] << 16) | (ctx.data[j + 2] << 8) | (ctx.data[j + 3]);
-				le_data[i] = le;
-			}
-
+			
 
             SHA256_CTX *d_ctx;
             Nonce_result *d_nr;
@@ -288,8 +323,8 @@ uint32_t device_mine_dispatcher(std::string payload, uint32_t difficulty, MineTy
 
             // 4194304 * 1024 = 0xffffffff + 1
 
-			dim3 gridDim(8192,8192);
-			// dim3 gridDim(1,1);
+			// dim3 gridDim(8192,8192);
+			dim3 gridDim(1,1);
 
 			dim3 blockDim(64,1);
             GPU_mine<<<gridDim, blockDim>>>(d_ctx, d_nr);
