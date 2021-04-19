3ad518ffb2f05e6e8ce35155577dda873bb5b941
Garry Kuwanto <gkuwanto@gmail.com>
2021-04-19 21:48:25 +0700

    a

--- src/util/blockchain.cpp
+++ src/util/blockchain.cpp
@@ -137,11 +137,13 @@ bool Block::verify_nonce(){
     uint32_t nDifficulty = this->candidate_block.getDifficulty();
     unsigned char difficulty[32];
     set_difficulty(difficulty, nDifficulty);
-    cout << hash<<endl<<difficulty;
     int i=0;
     while(hash[i] == difficulty[i])
         i++;
-    return (i>=nDifficulty);
+    if(hash[i] < difficulty[i]){
+        return true;
+    }
+    return false;
 }
 
 Blockchain::Blockchain() {
