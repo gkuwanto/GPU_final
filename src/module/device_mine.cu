6ba153aea470bd24acb8b28b593f0541545ff00a
Garry Kuwanto <gkuwanto@gmail.com>
2021-04-20 13:00:08 +0700

    typo

--- src/module/device_mine.cu
+++ src/module/device_mine.cu
@@ -281,7 +281,7 @@ uint32_t device_mine_dispatcher(std::string payload, uint32_t difficulty, MineTy
 				cudaMemcpy((void *) &result, dev_result, sizeof(uint32_t), cudaMemcpyDeviceToHost);
 				cudaDeviceSynchronize();
 				if (result != 0){
-					return result
+					return result;
 				}
 				current++;
 			}
