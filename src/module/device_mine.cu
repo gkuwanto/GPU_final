de05ac4a5c57a259228782c14021b26801dd9250
Garry Kuwanto <gkuwanto@gmail.com>
2021-04-20 12:29:21 +0700

    a

--- src/module/device_mine.cu
+++ src/module/device_mine.cu
@@ -9,7 +9,6 @@ using namespace std;
 
 template <size_t N>
 
-typedef uint8_t[32] sha256_hash;
 
 // SHA-2 uses big-endian integers.
 __device__ void write_u32(uint8_t* dest, uint32_t x)
@@ -61,7 +60,7 @@ __device__ uint64_t ror(uint64_t x, uint64_t n)
 // Both sha256_impl and sha512_impl are used by sha224/sha256 and
 // sha384/sha512 respectively, avoiding duplication as only the initial hash
 // values (s) and output hash length change.
-__device__ sha256_hash	sha256_impl(const uint32_t* s, const uint8_t* data, uint64_t length)
+__device__ uint8_t[32]	sha256_impl(const uint32_t* s, const uint8_t* data, uint64_t length)
 {
 	static_assert(sizeof(uint32_t) == 4, "sizeof(uint32_t) must be 4");
 	static_assert(sizeof(uint64_t) == 8, "sizeof(uint64_t) must be 8");
@@ -169,7 +168,7 @@ __device__ sha256_hash	sha256_impl(const uint32_t* s, const uint8_t* data, uint6
 		chunk((uint8_t *) buf, (uint32_t *) hash, (uint32_t *) k);
 	}
 
-	sha256_hash result;
+	uint8_t[32] result;
 
 	for (uint8_t i = 0; i != 8; ++i) {
 		write_u32(&result[i * 4], hash[i]);
@@ -178,7 +177,7 @@ __device__ sha256_hash	sha256_impl(const uint32_t* s, const uint8_t* data, uint6
 	return result;
 }
 
-__device__ sha256_hash	sha256(const uint8_t* data, uint64_t length)
+__device__ uint8_t[32]	sha256(const uint8_t* data, uint64_t length)
 {
 	// First 32 bits of the fractional parts of the square roots of the first
 	// eight primes 2..19:
@@ -245,7 +244,7 @@ __global__ void GPU_mine(const char* payload, uint32_t difficulty, uint32_t leng
 	data[length+7] = a[((nonce) % 16)];
 
 	auto ptr = reinterpret_cast<const uint8_t*>(data);
-	sha256_hash hash = sha256(ptr, length+8);
+	uint8_t[32] hash = sha256(ptr, length+8);
 	// uint32_t hash[32] = {0};
 	uint32_t i = 0;
 	while (hash[i]==0){
